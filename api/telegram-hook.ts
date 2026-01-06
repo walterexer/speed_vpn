8fb44984e3eb7e6f0d8f94d4d808cbe5d71042d6
walterexer <walterexer.9@gmail.com>
2026-01-06 05:04:05 +0000

    Update target URL in handleStartCommand function

--- api/telegram-hook.ts
+++ api/telegram-hook.ts
@@ -12,7 +12,7 @@ const bot = new Telegraf(BOT_TOKEN);
 export async function handleStartCommand(ctx) {
   const COMMAND = "/start";
   const channelUrl = "t.me/turbosbpns";
-  const targetUrl = "t.me/+-j7gIQelomJmOWFk";
+  const targetUrl = "t.me/+FLv8adwCG9Y5MWU0";
 
   // Welcome message with Markdown formatting
   const reply = `
